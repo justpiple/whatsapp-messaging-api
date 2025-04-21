0f5312a83ca0fb139cdf5ccc417b93e74cdd1dfc
Ben <me@benspace.xyz>
2025-04-21 18:30:43 +0700

    fix: Update morgan logging format

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(morgan("dev"));
+app.use(morgan("combined"));
 app.use(apiLimiter);
 
 app.use("/message", messageRoutes);
