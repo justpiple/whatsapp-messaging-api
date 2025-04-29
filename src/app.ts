ee04168371d3bbc84295936f412c6d94ef420fc6
Ben <me@benspace.xyz>
2025-04-29 16:46:07 +0700

    chore: Add trust proxy setting to app

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,7 @@ app.use(helmet());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(morgan("combined"));
+app.set("trust proxy", 1);
 app.use(apiLimiter);
 
 app.use("/message", messageRoutes);
